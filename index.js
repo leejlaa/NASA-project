f3b3ac8f1cadba0526146f690ef1489a771c0019
leejlaa <lejladoric00@gmail.com>
2023-10-02 21:48:59 +0200

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
